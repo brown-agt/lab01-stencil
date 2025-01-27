598501ddf6cabb820f57da5a008b7c4a48ee7639
John Wu <74892151+JohnUUU@users.noreply.github.com>
2025-01-27 10:03:38 -0500

    Update competition_agent.py

--- competition_agent.py
+++ competition_agent.py
@@ -1,8 +1,10 @@
 from agt_server.agents.base_agents.chicken_agent import ChickenAgent
 from agt_server.local_games.chicken_arena import ChickenArena
+
 class CompetitionAgent(ChickenAgent):
     def setup(self):
         self.SWERVE, self.CONTINUE = 0, 1
+        self.STUBBORN, self.COOPERATIVE = 1, 0
         self.actions = [self.SWERVE, self.CONTINUE]
 
     def get_action(self):
@@ -14,7 +16,7 @@ class CompetitionAgent(ChickenAgent):
 
 if __name__ == "__main__":
     #### PLEASE EDIT THESE VARIABLES #####
-    agent_name = ... # TODO: Please give your agent a name
+    agent_name = ??? # TODO: Please give your agent a name
     ip = ... # TODO: Please ask your Lab TA for the IP of the server
     port = ... # TODO: Please ask your Lab TA for the correct port
     join_server = False # TODO: Set this to True if you want to join the server rather than run a local game against yourself
